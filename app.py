214e1ce4cd7bcd9b702d86a68f800d88bf5a560c
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-06-05 22:53:35 +0530

    fix deploy entry point

--- app.py
+++ app.py
@@ -163,4 +163,5 @@ def create_app(config_class=None):
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
